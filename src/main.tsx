acf7540504c8f562768a3f9a02922c9e6e1519fb
Vikash Chauhan <vikashchauhan51@gmail.com>
2025-03-01 15:07:13 +0530

    removed defualt css

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Theme, ThemePanel } from "@radix-ui/themes";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Theme accentColor='jade' radius="small" panelBackground="translucent" appearance="inherit">
     <App />
+    <ThemePanel />
+    </Theme>
   </StrictMode>,
 )
